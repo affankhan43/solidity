9cc5f0d1268e557d1b249c1f8530d9f4c23eb787
affankhan43 <affankhan627@gmail.com>
2021-03-11 15:32:46 +0500

    update

--- deploy.js
+++ deploy.js
@@ -9,17 +9,26 @@ var web3 = new Web3(provider)
 
 async function deploy(){
 	var account  = await web3.eth.getAccounts();
-	console.log(account)	
+	//console.log(account)	
 	var balance = await web3.eth.getBalance(account[0])
 	console.log(web3.utils.fromWei(balance, 'ether'))
-	console.log('-----------------------------------')
-	console.log('contract deployment using account---->' + account[0])
-	inbox = await new web3.eth.Contract(abi)
-	.deploy({'data':bytecode,'arguments':['Hi']})
-	.send({
-		from:account[0],
-		gas: '547611',
-		gasPrice: '30000000000'})
-	console.log('contract deployed on address---->' + inbox.options.address)
+	// console.log('-----------------------------------')
+	// console.log('contract deployment using account---->' + account[0])
+	// inbox = await new web3.eth.Contract(abi)
+	// .deploy({'data':bytecode,'arguments':['Hi']})
+	// .send({
+	// 	from:account[0],
+	// 	gas: '547611',
+	// 	gasPrice: '30000000000'})
+	// console.log('contract deployed on address---->' + inbox.options.address)
 }
-deploy()
+
+async function Players() {
+	var account = await web3.eth.getAccounts();
+	lottery = await new web3.eth.Contract(abi,"0x39fcd315a6168633b7aba52cd9d30ae55fa4bec1")
+	var message = await lottery.methods.setMessage("This is my new Message").send({"from":account[0]})
+	//var message = await lottery.methods.getMessage().call()
+	console.log(message)
+}
+Players()
+//deploy()
